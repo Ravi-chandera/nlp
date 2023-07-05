fcccf78eea9a7ab6be5732ebcca73ad49fbbe69a
Ravi-chandera <110690059+Ravi-chandera@users.noreply.github.com>
2023-07-05 19:25:45 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import pandas as pd
 
 st.set_page_config(
     page_title = "multiple pages",
-    page_icon = "🤣",
+    # page_icon = "🤣",
 )
 st.title("Main page")
 st.sidebar.success("Select above page")
